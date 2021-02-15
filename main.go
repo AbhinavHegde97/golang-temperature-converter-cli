be6036e62fceed0709757d0f0fa7552803eab43c
abhinav_hegde <hegdeabhinav7@gmail.com>
2021-02-15 23:30:22 +0530

    Making changes according to docs
    Adding check for number of params passed from the cmd line

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strings"
 )
 
 var originUnit string
@@ -18,15 +19,20 @@ var errReadingInput = errors.New("Error reading input")
 
 func main() {
 
-	for {
-		fmt.Print("What is the current temperature in " + originUnit + " ? ")
+	if len(os.Args) == 2 {
+		originUnit = strings.ToUpper(os.Args[1])
+		for {
+			fmt.Print("What is the current temperature in " + originUnit + " ? ")
 
-		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+			fmt.Print("Would you like to convert another temperature ? (y/n) ")
 
-		if shouldConvertAgain != "Y" {
-			fmt.Println("Good bye!")
-			break
+			if shouldConvertAgain != "Y" {
+				fmt.Println("Good bye!")
+				break
+			}
 		}
+	} else {
+		printError(errInvalidArguments)
 	}
 }
 
